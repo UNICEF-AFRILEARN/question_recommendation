9f74fb1b94d30bb0684e2eea125bedb4e57f21da
Israel Igietsemhe <alo.momodu@mail.utoronto.ca>
2023-06-28 11:43:18 -0400

    random user table

--- recommender.py
+++ recommender.py
@@ -1,6 +1,7 @@
 from bson import ObjectId
 import pandas as pd
 import pickle
+import random
 
 
 
@@ -21,41 +22,44 @@ def get_recommendations(userId,courseId,n_questions,rec_type=None,lessonId=None,
     
     label_transforms =  pickle.load(open('encoders.pkl','rb'))
     label_encoders = label_transforms["label"+str(class_label)]
-    le_userId = label_encoders[2]
-    userId_encoded = le_userId.transform([userId])[0]
-    
-    unattempted_questions = responses_df[(responses_df["userId"]==userId_encoded)&(responses_df["number_of_attempts"]==0)]
-    if len(unattempted_questions)==0:
-        unattempted_questions = responses_df[(responses_df["userId"]==userId_encoded)]
 
-    models =  pickle.load(open('classifiers.pkl','rb'))
-    model = models["classifier"+str(class_label)]
-    unattempted_questions["prob_correct"] = get_prob_correct(model, unattempted_questions)
-    
-    le_subjectname = label_encoders[1]
-    le_questionId = label_encoders[3]
-    if rec_type=="lesson":
-        if lessonId:
-            unattempted_questions["questionId"] = unattempted_questions["questionId"].apply(lambda x: le_questionId.inverse_transform([x]))
-            questions = pickle.load(open('questions.pkl','rb'))
-            lesson_recommendation_df = questions.merge(unattempted_questions,left_on=['_id'],right_on=['questionId'],how='right')
-            lesson_recommendation_df= lesson_recommendation_df[lesson_recommendation_df["lessonId"]==ObjectId(lessonId)]
-            lesson_recommendation_df.sort_values(by="prob_correct",ascending=False,inplace = True)
-            recommended_questions = lesson_recommendation_df['questionId'].values[:n_questions]
+    if userId:
+        le_userId = label_encoders[2]
+        userId_encoded = le_userId.transform([userId])[0]
+        
+        unattempted_questions = responses_df[(responses_df["userId"]==userId_encoded)&(responses_df["number_of_attempts"]==0)]
+        if len(unattempted_questions)==0:
+            unattempted_questions = responses_df[(responses_df["userId"]==userId_encoded)]
+
+        models =  pickle.load(open('classifiers.pkl','rb'))
+        model = models["classifier"+str(class_label)]
+        unattempted_questions["prob_correct"] = get_prob_correct(model, unattempted_questions)
+        
+        le_subjectname = label_encoders[1]
+        le_questionId = label_encoders[3]
+        if rec_type=="lesson":
+            if lessonId:
+                unattempted_questions["questionId"] = unattempted_questions["questionId"].apply(lambda x: le_questionId.inverse_transform([x]))
+                questions = pickle.load(open('questions.pkl','rb'))
+                lesson_recommendation_df = questions.merge(unattempted_questions,left_on=['_id'],right_on=['questionId'],how='right')
+                lesson_recommendation_df= lesson_recommendation_df[lesson_recommendation_df["lessonId"]==ObjectId(lessonId)]
+                lesson_recommendation_df.sort_values(by="prob_correct",ascending=False,inplace = True)
+                recommended_questions = lesson_recommendation_df['questionId'].values[:n_questions]
+            else:
+                return print("Enter a lessonId to get question recommendations for this lesson.")
+        elif rec_type=="subject":
+            if subject_name:
+                subject_name = le_subjectname.transform([subject_name])[0]
+                subject_recommendation_df= unattempted_questions[unattempted_questions["subjectId"]==subject_name]
+                subject_recommendation_df.sort_values(by="prob_correct",ascending=False,inplace = True)
+                recommended_questions = subject_recommendation_df['questionId'].values[:n_questions]
+            else:
+                return print("Enter a subject name to get question recommendations for this subject.")
         else:
-            return print("Enter a lessonId to get question recommendations for this lesson.")
-    elif rec_type=="subject":
-        if subject_name:
-            subject_name = le_subjectname.transform([subject_name])[0]
-            subject_recommendation_df= unattempted_questions[unattempted_questions["subjectId"]==subject_name]
-            subject_recommendation_df.sort_values(by="prob_correct",ascending=False,inplace = True)
-            print(subject_recommendation_df)
-            recommended_questions = subject_recommendation_df['questionId'].values[:n_questions]
-        else:
-            return print("Enter a subject name to get question recommendations for this subject.")
-    else:
-        unattempted_questions.sort_values(by="prob_correct",ascending=False,inplace = True)
-        recommended_questions = unattempted_questions['questionId'].values[:n_questions]
+            unattempted_questions.sort_values(by="prob_correct",ascending=False,inplace = True)
+            recommended_questions = unattempted_questions['questionId'].values[:n_questions]
+    else: 
+        recommended_questions=random.choices(list(responses_df['questionId'].unique()),k=10)
     recommended_questions  = le_questionId.inverse_transform(recommended_questions)
     return recommended_questions
  
